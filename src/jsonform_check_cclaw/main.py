9f4d7aaec97e2d97faaeba87024ea61ea0ab1107
ymh <hanyongming@gmail.com>
2023-11-13 19:19:59 +0800

    chore: make hypothesis settings configurable from config.yaml; add structlog as dep

--- src/jsonform_check_cclaw/main.py
+++ src/jsonform_check_cclaw/main.py
@@ -4,18 +4,18 @@ from pathlib import Path
 from dataclasses import dataclass
 from typing_extensions import Annotated
 from typer_config.decorators import use_yaml_config  
+from init_hyp import hyp_settings_load_profile
 
 app = typer.Typer()
 
+# TODO: Think more about what should be optional -- not obvious that all of these are best modelled with Optional
 @dataclass
 class AppConfig:
     form_schema_path: Optional[Path] = None
     base_profiles_path: Optional[Path] = None
     form_app_path: Optional[Path] = None
     le_server_path: Optional[str] = None
-    app_mode: str = "dev"
-
-
+    hypothesis_settings: Optional[str] = None
 
 # @app.command("check-base-profiles")
 # def check_base_profiles(
@@ -36,13 +36,15 @@ def main(
     base_user_profiles: Annotated[Path, typer.Option()],
     form_app: Annotated[Path, typer.Option()],
     le_server: Annotated[str, typer.Option()],
-    mode: Annotated[str, typer.Option()]
+    hypothesis_profile: Annotated[str, typer.Option()],
 ):
+    hypothesis_settings = hyp_settings_load_profile(hypothesis_profile)  
     app_config = AppConfig(form_schema_path=form_schema,
                            base_profiles_path=base_user_profiles,
                            form_app_path=form_app,
                            le_server_path=le_server,
-                           app_mode=mode)  
+                           hypothesis_settings=hypothesis_settings)
+    
     typer.echo(f"{app_config}")
 
 if __name__ == "__main__":
